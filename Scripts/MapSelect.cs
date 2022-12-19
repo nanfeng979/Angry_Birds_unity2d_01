8449db868b8b4d1b5e7494558ae1e0209b6d88e0
nanfeng979 <306338401@qq.com>
2022-12-19 14:18:14 +0800

    增加关卡页面的点击功能，从扇叶到关卡的切换

--- Scripts/MapSelect.cs
+++ Scripts/MapSelect.cs
@@ -5,20 +5,31 @@ using UnityEngine;
 public class MapSelect : MonoBehaviour
 {
     public int starsCount = 0;
-    public bool canSelct = false;
+    public bool canSelect = false;
 
     public GameObject lock_;
     public GameObject stars;
 
+    public GameObject panel;
+
+    public GameObject map;
 
     private void Start() {
         if(PlayerPrefs.GetInt("totalNum", 0) >= starsCount) {
-            canSelct = true;
+            canSelect = true;
         }
 
-        if(canSelct) {
+        if(canSelect) {
             lock_.SetActive(false);
             stars.SetActive(true);
+        }
+    }
+
+    // 鼠标点击方法
+    public void Select() {
+        if(canSelect) {
+            panel.SetActive(true);
+            map.SetActive(false);
         }
     }
 }
