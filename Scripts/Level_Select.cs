251361b6a8e5259144e0fc4b3aafd4d7565e16ca
nanfeng979 <306338401@qq.com>
2022-12-19 12:06:36 +0800

    增加关卡选择按钮

---
+++ Scripts/Level_Select.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level_Select : MonoBehaviour
+{
+
+    public bool canSelect = false;
+
+    public Sprite levelBg;
+
+    private Image image;
+
+    private void Awake() {
+        image = GetComponent<Image>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(transform.parent.GetChild(0).name == gameObject.name) {
+            canSelect = true;
+        }
+
+        if(canSelect) {
+            image.overrideSprite = levelBg;
+            transform.Find("num").gameObject.SetActive(true);
+        }
+    }
+
+}
